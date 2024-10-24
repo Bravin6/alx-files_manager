e1c8b438c9b4786835b81db17aeb0929f08492f3
Bravin6 <136716286+Bravin6@users.noreply.github.com>
2024-10-24 04:24:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-
-<<<<<<< HEAD
 const express = require('express');
 const routes = require('./routes'); // Load routes from routes/index.js
 
 const app = express();
 const port = process.env.PORT || 5000; // Use PORT from env or default to 5000
 
-// Use routes
-app.use('/', routes);
+app.use(express.json({ limit: '100mb' })); // Use JSON parser with limit
+app.use('/', routes); // Load routes
 
 // Start server
 app.listen(port, () => {
@@ -15,14 +13,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-=======
-import express from 'express';
-import router from './routes';
-
-const app = express();
-app.use(express.json({ limit: '100mb' }));
-app.use(router);
-app.listen(process.env.PORT || 5000);
-
-export default app;
->>>>>>> cc971b572d16c3433e79ddc3a05771323fc21b83
