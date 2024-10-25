324b06a30278b98eb053c144165f611f9a0c1733
Bravin6 <136716286+Bravin6@users.noreply.github.com>
2024-10-25 06:51:16 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
 
-// Existing endpoints
+// Create a new router instance
+const router = Router();
+
+// Define the endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// New endpoints
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files', FilesController.getIndex);
-router.get('/files/:id', FilesController.getShow);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
-
-module.exports = router;
+export default router;
